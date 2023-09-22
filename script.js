7dbf940dd2a5a8a01dc9be9084f76c7f6f9f0ffb
arahman-html <139979816+arahman-html@users.noreply.github.com>
2023-09-22 23:40:29 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// JavaScript code for the click button functionality
+document.addEventListener('DOMContentLoaded', function() {
+    const clickButton = document.querySelector('.click-button');
+  
+    clickButton.addEventListener('click', function() {
+      alert('Button clicked!');
+    });
+  });
+  
+
+
